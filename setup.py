2bfbdaea69b2627c0561e0bf13c1f76fe66643e8
Batuhan Türker <57283569+batuhanturker@users.noreply.github.com>
2024-06-21 16:34:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,59 @@
 import subprocess
+import sys
+
+def run_command(command, description):
+    print(f"Starting: {description}...")
+    try:
+        subprocess.run(command, check=True)
+        print(f"Completed: {description}.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred during {description}: {e}")
+        sys.exit(1)
 
 def install_packages():
-    try:
-        subprocess.run(["apt-get", "install", "hostapd", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "dnsmasq", "-y"], check=True)
-    except subprocess.CalledProcessError as e:
-        print("Error occurred during package installation:", e)
-        exit(1)
+    run_command(["apt-get", "update"], "updating package lists")
+    run_command(["apt-get", "install", "hostapd", "-y"], "installing hostapd")
+    run_command(["apt-get", "install", "dnsmasq", "-y"], "installing dnsmasq")
 
 def setup_web_server():
-    try:
-        subprocess.run(["sudo", "rm", "-rf", "/var/www/html"], check=True)
-        subprocess.run(["cp", "-r", "html", "/var/www/"], check=True)
-    except subprocess.CalledProcessError as e:
-        print("Error occurred during web server setup:", e)
-        exit(1)
+    run_command(["sudo", "rm", "-rf", "/var/www/html"], "removing existing web files")
+    run_command(["cp", "-r", "html", "/var/www/"], "copying new web files")
+
+def show_banner():
+    banner1 = """
+   ___________     .__.__    ___________       .__        
+   \_   _____/__  _|__|  |   \__    ___/_  _  _|__| ____  
+   |    __)_\  \/ /  |  |     |    |  \ \/ \/ /  |/    \ 
+   |        \\   /|  |  |__   |    |   \     /|  |   |  \\
+  /_______  / \_/ |__|____/   |____|    \/\_/ |__|___|  /
+          \/                                          \/ 
+  __________                   __               __       
+  \______   \_______  ____    |__| ____   _____/  |_     
+   |     ___/\_  __ \/  _ \   |  |/ __ \_/ ___\   __\    
+   |    |     |  | \(  <_> )  |  \  ___/\  \___|  |      
+   |____|     |__|   \____/\__|  |\___  >\___  >__|      
+                          \______|    \/     \/                                       
+"""
+    banner2 = """
+   EVIL TWIN PROJECT
+   By https://github.com/batuhanturker, 
+      https://github.com/aykutemreyalcin, 
+      https://github.com/fdemirhisar 
+"""
+    print(banner1)
+    print(banner2)
+
+def main():
+    show_banner()
+    print("Script started.")
+    
+    print("\033[1;34;40mInstalling necessary packages...\033[0m")
+    install_packages()
+    
+    print("\033[1;34;40mSetting up web server...\033[0m")
+    setup_web_server()
+    
+    print("\033[1;32mSetup completed successfully.\033[0m")
 
 if __name__ == "__main__":
-    print("Installing packages...")
-    install_packages()
-    print("Setting up web server...")
-    setup_web_server()
-    print("Setup completed successfully.")
+    main()
